79af41730d77484c88976bee2fc2bb87b68debaf
Delph <49454418+Delph@users.noreply.github.com>
2021-04-20 08:43:41 +0100

    Improved results screen layout (including mobile)

--- src/screens/Results.jsx
+++ src/screens/Results.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { connect } from 'react-redux';
 
-import { number_format } from '../util';
+import { number_format, activated_effect } from '../util';
+
+import style from '../css/screens/Results.module.css';
 
 import fire from '../img/elem/fire.png';
 import water from '../img/elem/water.png';
@@ -10,68 +12,108 @@ import thunder from '../img/elem/thunder.png';
 import ice from '../img/elem/ice.png';
 import dragon from '../img/elem/dragon.png';
 
-function Row({set}) {
-  const torso_inc = set.combination.filter(g => g.torso_inc).length;
+function Row({search, set}) {
+  const chosen_effects = useMemo(() => search.effects.map(e => activated_effect(e.skill, e.points)), [search]);
+
+  const extra_skills = Object.keys(set.skills).filter(s => !chosen_effects.includes(activated_effect(s, set.skills[s])));
+
+  const torso_decorations = set.chest_decorations.reduce((a, c) => { a[c.name] = (a[c.name] ?? 0) + 1; return a; }, {});
+  const decorations = set.decorations.reduce((a, c) => { a[c.name] = (a[c.name] ?? 0) + 1; return a; }, {});
 
   return (
-    <tr>
-      <td title={`Effective ${set.eff.raw}`}>{number_format(set.raw.raw)}</td>
-      <td title={`Effective ${set.eff.fire}`}>{number_format(set.raw.fire)}</td>
-      <td title={`Effective ${set.eff.water}`}>{number_format(set.raw.water)}</td>
-      <td title={`Effective ${set.eff.thunder}`}>{number_format(set.raw.thunder)}</td>
-      <td title={`Effective ${set.eff.ice}`}>{number_format(set.raw.ice)}</td>
-      <td title={`Effective ${set.eff.dragon}`}>{number_format(set.raw.dragon)}</td>
-      <td>{set.combination[0].name}</td>
-      <td>{set.combination[1].name}</td>
-      <td>{set.combination[2].name}</td>
-      <td>{set.combination[3].name}</td>
-      <td>{set.combination[4].name}</td>
-      <td>
-        {torso_inc ? `Chest: ${set.chest_decorations.map(d => d.name).join(', ')}` : ''}
-        {set.decorations.map(d => d.name).join(', ')}
+    <tr className={style.row}>
+      <td className={style.desktop} title={`Effective ${set.eff.raw}`}>{number_format(set.raw.raw)}</td>
+      <td className={style.desktop} title={`Effective ${set.eff.fire}`}>{number_format(set.raw.fire)}</td>
+      <td className={style.desktop} title={`Effective ${set.eff.water}`}>{number_format(set.raw.water)}</td>
+      <td className={style.desktop} title={`Effective ${set.eff.thunder}`}>{number_format(set.raw.thunder)}</td>
+      <td className={style.desktop} title={`Effective ${set.eff.ice}`}>{number_format(set.raw.ice)}</td>
+      <td className={style.desktop} title={`Effective ${set.eff.dragon}`}>{number_format(set.raw.dragon)}</td>
+      <td className={style.mobile}>
+        <div>{number_format(set.raw.raw)}</div>
+        <div>{number_format(set.raw.fire)}</div>
+        <div>{number_format(set.raw.water)}</div>
+        <div>{number_format(set.raw.thunder)}</div>
+        <div>{number_format(set.raw.ice)}</div>
+        <div>{number_format(set.raw.dragon)}</div>
       </td>
-      <td>{JSON.stringify(set.skills)}</td>
+      <td className={style.desktop}>{set.combination[0].name}</td>
+      <td className={style.desktop}>{set.combination[1].name}</td>
+      <td className={style.desktop}>{set.combination[2].name}</td>
+      <td className={style.desktop}>{set.combination[3].name}</td>
+      <td className={style.desktop}>{set.combination[4].name}</td>
+      <td className={style.mobile}>{set.combination.map(g => <div>{g.name}</div>)}</td>
+      <td>
+        {extra_skills.map(s => <div key={s} className={set.skills[s] < 0 ? style.bad : style.good}>{activated_effect(s, set.skills[s])}</div>)}
+      </td>
+      <td>
+        {set.torso_inc ? `Chest: ${Object.keys(torso_decorations).map(d => `${torso_decorations[d]} ${d}${(torso_decorations[d] > 1 ? 's' : '')}`).join(', ')}` : ''}<br/>
+        {Object.keys(decorations).map(d => `${decorations[d]} ${d}${(decorations[d] > 1 ? 's' : '')}`).join(', ')}
+      </td>
+      <td>{JSON.stringify(set.slots)}</td>
     </tr>
   );
 }
 
 function Results(props) {
-  const { results, pagination, next, prev } = props;
+  const { search, results, pagination, next, prev } = props;
 
+  const max = Math.min(pagination.offset + pagination.count, results.length);
   return (
-    <React.Fragment>
-      <input type="button" value="Prev" onClick={prev} disabled={pagination.offset === 0}/>
-      <input type="button" value="Next" onClick={next} disabled={pagination.offset + 10 >= results.length}/>
-      <span>Showing {number_format(pagination.offset)} to {number_format(pagination.offset + pagination.count)} of {number_format(results.length)} results.</span>
-      <table>
-        <thead>
-          <tr>
-            <th>Defence</th>
-            <th><img style={{width: 16}} src={fire} alt={'Fire'}/></th>
-            <th><img style={{width: 16}} src={water} alt={'Water'}/></th>
-            <th><img style={{width: 16}} src={thunder} alt={'Thunder'}/></th>
-            <th><img style={{width: 16}} src={ice} alt={'Ice'}/></th>
-            <th><img style={{width: 16}} src={dragon} alt={'Dragon'}/></th>
-            <th>Head</th>
-            <th>Chest</th>
-            <th>Arms</th>
-            <th>Waist</th>
-            <th>Legs</th>
-            <th>Decorations</th>
-            <th>Slots</th>
-          </tr>
-        </thead>
-        <tbody>
-        {results.slice(pagination.offset, pagination.offset + pagination.count).map((r, i) => <Row key={i} set={r}/>)}
-        </tbody>
-      </table>
-    </React.Fragment>
+    <div className={style.container}>
+      <div className={style.top}>
+        <fieldset className={style.search}>
+          <legend>Search</legend>
+          <div>
+            {search.effects.map(e => <div>{activated_effect(e.skill, e.points)}</div>)}
+          </div>
+          <div>
+            <div>VR: {search.vr}</div>
+            <div>HR: {search.hr}</div>
+          </div>
+        </fieldset>
+        <fieldset>
+          <div>
+            <input type="button" value="Prev" onClick={prev} disabled={pagination.offset === 0}/>
+            <input type="button" value="Next" onClick={next} disabled={pagination.offset + 10 >= results.length}/>
+          </div>
+          <div>Showing {number_format(pagination.offset)} to {number_format(max)} of {number_format(results.length)} results.</div>
+        </fieldset>
+      </div>
+      <div className={style.tableContainer}>
+        <table className={style.table}>
+          <thead>
+            <tr>
+              <th className={`${style.head} ${style.desktop}`}>Defence</th>
+              <th className={`${style.head} ${style.desktop}`}><img style={{width: 16}} src={fire} alt={'Fire'}/></th>
+              <th className={`${style.head} ${style.desktop}`}><img style={{width: 16}} src={water} alt={'Water'}/></th>
+              <th className={`${style.head} ${style.desktop}`}><img style={{width: 16}} src={thunder} alt={'Thunder'}/></th>
+              <th className={`${style.head} ${style.desktop}`}><img style={{width: 16}} src={ice} alt={'Ice'}/></th>
+              <th className={`${style.head} ${style.desktop}`}><img style={{width: 16}} src={dragon} alt={'Dragon'}/></th>
+              <th className={`${style.head} ${style.mobile}`}>Defence</th>
+              <th className={`${style.head} ${style.desktop}`}>Head</th>
+              <th className={`${style.head} ${style.desktop}`}>Torso</th>
+              <th className={`${style.head} ${style.desktop}`}>Arms</th>
+              <th className={`${style.head} ${style.desktop}`}>Waist</th>
+              <th className={`${style.head} ${style.desktop}`}>Legs</th>
+              <th className={`${style.head} ${style.mobile}`}>Equipment</th>
+              <th className={style.head}>Extra Skills</th>
+              <th className={style.head}>Decorations</th>
+              <th className={style.head}>Slots</th>
+            </tr>
+          </thead>
+          <tbody>
+          {results.slice(pagination.offset, pagination.offset + pagination.count).map((r, i) => <Row key={i} search={search} set={r}/>)}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
 
 function mapStateToProps(state) {
   return {
+    search: state.search,
     results: state.results.sets,
     pagination: state.results.pagination
   };
--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+import { skills } from './gamedata';
+
 function number_format(x)
 {
   return new Intl.NumberFormat().format(x);
@@ -8,4 +10,20 @@ function money_format(x)
   return `${new Intl.NumberFormat().format(x)}z`;
 }
 
-export { number_format, money_format };
+function activated_effect(name, points)
+{
+  const skill = skills.find(s => s.name === name);
+  if (!skill)
+    return null;
+
+  while (points !== 0)
+  {
+    const effect = skill.skills[Math.trunc(points / 5) * 5];
+    if (effect)
+      return effect;
+    points = (Math.trunc(points / 5) + (points < 0 ? 1 : -1)) * 5;
+  }
+  return null;
+}
+
+export { number_format, money_format, activated_effect };
